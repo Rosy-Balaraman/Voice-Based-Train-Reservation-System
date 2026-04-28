d9862a88424d652ac1d9fe80bfffa097d41e6158
Rosy-Balaraman <rosyb.0526@gmail.com>
2026-04-28 11:41:15 +0530

    forgetpass.php

--- my project/forgetpass.php
+++ my project/forgetpass.php
@@ -29,8 +29,9 @@ if (isset($_POST['send_email']))
             $mail->isSMTP();
             $mail->Host = 'smtp.gmail.com';
             $mail->SMTPAuth = true;
-            $mail->Username = 'cheerfulljourney.rrs@gmail.com';
-            $mail->Password = 'dpgl xdaf clbf gyvv'; 
+            // For security reasons, the email and password will be hidden.
+            $mail->Username = '';
+            $mail->Password = ''; 
             $mail->SMTPSecure = 'tls';
             $mail->Port = 587;
 
